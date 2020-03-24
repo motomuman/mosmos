f8d97e135a99265ffe8f364a2ef362c2f57eb775
Motomu Utsumi <motomuman@gmail.com>
2020-03-24 23:05:29 +0900

    Fixed kernel image, jump to kentry in kasm.s

--- kernel/kasm.s
+++ kernel/kasm.s
@@ -2,11 +2,23 @@ section .text
 
 [BITS 32]
 
-global _kentry
+global kentry
+global _write
+global _hlt
+
 extern _kstart
 
-_kentry:
-	cli
-	call	_kstart
+kentry:
+	call 	_kstart
 
-	jmp	$
+
+_write:
+	mov     [0x000A_0f00 + 80 + 5], byte 0xFF
+	mov     [0x000A_0f00 + 160 + 5], byte 0xFF
+	mov     [0x000A_0f00 + 240 + 5], byte 0xFF
+	mov     [0x000A_0f00 + 320 + 5], byte 0xFF
+	ret
+
+_hlt:
+	hlt
+	ret
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,34 +1,65 @@
+void write();
+void hlt();
+
 void kstart(void)
 {
 	int i;
 	char *p;
 
-	for (i = 0xa0000; i <= 0xaffff; i += 8) {
-		p = (char *) i;
-		*p = 0xff;
+	// width: 640 bit, height: 480 bit. Each row has 80 bytes (640 bits)
+	//
+	// 0x000A_0000 + (640/8) x 0 +------+------+- ... -+------+
+	// 0x000A_0000 + (640/8) x 1
+	// 0x000A_0000 + (640/8) x 2
+	//
+
+
+	// Each character has 8 bit width and 16 bit height.
+	// So we can have 80 characters in one line
+	// and can have 30 lines.
+	//
+	//                  +0     +1             +79
+	// + (80 x 16) x 0 +------+------+- ... -+------+
+	//                 |      |
+	//                 |      |
+	//                 |      |
+	//                 |      |
+	// + (80 x 16) x 1 +------+------+- ... -+------+
+	//                 |      |
+	//                 |      |
+	//                 |      |
+	//                 |      |
+	//
+	for (i = 0; i <= 100000000; i++) {
 	}
 
-	for (i = 0; i <= 1000000000; i++) {
-	}
-	for (i = 0xa0000; i <= 0xaffff; i += 8) {
+	for (i = 0xa0000; i <= 0xaffff; i += 1) {
 		p = (char *) i;
 		*p = 0x00;
 	}
 
-	for (i = 0; i <= 1000000000; i++) {
-	}
-	for (i = 0xa0000; i <= 0xaffff; i += 8) {
-		p = (char *) i;
-		*p = 0xf0;
+	write();
+
+	for (i = 0; i <= 100000000; i++) {
 	}
 
-	for (i = 0; i <= 1000000000; i++) {
+	for (i = 0xa0000; i <= 0xaffff; i += 1) {
+		p = (char *) i;
+		*p = 0x00;
 	}
+
+	for (i = 0; i <= 100000000; i++) {
+	}
+
+	write();
+
+
 	for (i = 0xa0000; i <= 0xaffff; i += 8) {
 		p = (char *) i;
 		*p = 0x0f;
 	}
 
 	while(1){
+		hlt();
 	}
 }
